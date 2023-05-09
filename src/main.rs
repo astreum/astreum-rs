b43e07f08c5eb74287136e9779ddaba10a5fb800
Roy Okello <royokello@protonmail.com>
2023-05-09 12:40:58 +0300

    v0.1.0
    - create account
    - help
    
    Resolves #1, Resolves #3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, env, path::Path, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    println!("Astreum Rust v0.1.0");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+
+        let topic : &str = &args[1];
+
+        match topic {
+
+            "new" => {
+
+                println!("Creating Account ...");
+
+                let secret_key = fides::ed25519::secret_key();
+
+                let public_key = fides::ed25519::public_key(&secret_key)?;
+
+                let public_key_hex = hex::encode(&public_key);
+
+                if !Path::new("./keys").exists() { fs::create_dir("./keys")?; };
+
+                fs::write(format!("./keys/{}.bin", public_key_hex), &secret_key)?;
+
+                println!("Done, Account Address is {}", public_key_hex);
+
+            },
+
+            _ => help()
+
+        }
+
+    } else {
+        
+        help()
+
+    }
+
+    Ok(())
+    
+}
+
+fn help() {
+
+    println!(r###"
+Help
+- - - + - - - + - - -
+
+new     creates a new account
+
+    "###);
+
 }
