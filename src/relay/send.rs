54bb6bc8b1b173c7429e01baffbd4b630f954aea
Roy Okello <royokello@protonmail.com>
2023-05-14 19:40:40 +0300

    Update send.rs
    Resolves #17

--- src/relay/send.rs
+++ src/relay/send.rs
@@ -1,9 +1,25 @@
-use std::net::IpAddr;
+use std::{net::IpAddr, error::Error};
 
 use super::{Relay, message::Message};
 
 impl Relay {
 
-   pub fn send(ip_address: IpAddr, message: Message) {}
+   pub fn send(&self, ip_address: IpAddr, message: Message) -> Result<(), Box<dyn Error>> {
+
+      match self.outgoing_queue_pointer.lock() {
+         
+         Ok(mut outgoing_queue) => {
+            
+            outgoing_queue.push((ip_address, message));
+
+            Ok(())
+
+         },
+        
+         Err(_) => Err("outgoing queue pointer lock error!")?
+    
+      }
+
+   }
 
 }
